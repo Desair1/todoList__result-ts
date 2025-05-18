b048c3ac2707df8ef3a269441e94fc1cdd02cff5
Desair1 <ilya_velichko_1995@mail.ru>
2025-05-18 17:13:20 +0300

    added 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TodoList from "./components/TodoList";
 import TaskDetails from "./components/TaskDetails";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<TodoList />} />
         <Route path="/todos/:id" element={<TaskDetails />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
